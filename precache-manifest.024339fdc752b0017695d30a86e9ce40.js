439986b01f2b3259644d52990579a95f942ce468
James Macmillan <jamescodesthings@gmail.com>
2020-01-31 14:37:51 +0000

    Updates

---
+++ precache-manifest.024339fdc752b0017695d30a86e9ce40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede59c8beadccea8aa3f67f75844b9a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba40bda9e634dc5c296a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e65b0bf15f3e1e307950",
+    "url": "/static/js/2.c34f79a2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c34f79a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba40bda9e634dc5c296a",
+    "url": "/static/js/main.07bbffc0.chunk.js"
+  },
+  {
+    "revision": "9108e1c2d06879ae1b4e",
+    "url": "/static/js/runtime-main.e5df69b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
