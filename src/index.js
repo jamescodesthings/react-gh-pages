66af2383a6d1d248233b416e494a80075dae147f
James Macmillan <jamescodesthings@gmail.com>
2020-02-02 17:14:07 +0000

    React axe and Eslint fixes

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if (process.env.NODE_ENV !== 'production') {
+  // Import axe and warn
+  (async () => {
+    const axe = await import('react-axe');
+    axe.default(React, ReactDOM, 1000);
+  })();
+}
